5b85d2c75229be8850419f6974bf2182a602e608
Jhonatan <jhonatancardoso09@gmail.com>
2020-06-11 18:09:31 -0300

    Updates

---
+++ precache-manifest.140aa6f8f6c8bfe04ee40811f6a04df5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301983b5deb1337d5ad3f7981674c34e",
+    "url": "/UolTest/index.html"
+  },
+  {
+    "revision": "d48d2da4362325082464",
+    "url": "/UolTest/static/css/2.a2c6895a.chunk.css"
+  },
+  {
+    "revision": "6006d09d04887ac49390",
+    "url": "/UolTest/static/css/main.ba9d8231.chunk.css"
+  },
+  {
+    "revision": "d48d2da4362325082464",
+    "url": "/UolTest/static/js/2.d434270f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/UolTest/static/js/2.d434270f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6006d09d04887ac49390",
+    "url": "/UolTest/static/js/main.e806c6ea.chunk.js"
+  },
+  {
+    "revision": "da9825c3f50ebc7647ca",
+    "url": "/UolTest/static/js/runtime-main.de742ed6.js"
+  },
+  {
+    "revision": "fe6f12e2cb5018339eb09a97c3904452",
+    "url": "/UolTest/static/media/bg-top.fe6f12e2.jpg"
+  },
+  {
+    "revision": "d01ca14fa96782ea2256d7468451ea11",
+    "url": "/UolTest/static/media/leagues-1.d01ca14f.png"
+  },
+  {
+    "revision": "68a60f1f6dfe1276d71a3a5c3706ecba",
+    "url": "/UolTest/static/media/leagues-2.68a60f1f.png"
+  },
+  {
+    "revision": "2d4af7d8f0a81cdf3334220e7b61cd81",
+    "url": "/UolTest/static/media/logo-EI.2d4af7d8.png"
+  },
+  {
+    "revision": "bd42bd8cf26d599d3ea0f4c99ca09359",
+    "url": "/UolTest/static/media/logo-espn.bd42bd8c.png"
+  },
+  {
+    "revision": "2137c820a527027aa581515a07fb7cb1",
+    "url": "/UolTest/static/media/logo-uol-init.2137c820.png"
+  },
+  {
+    "revision": "c4dc884df488fd9c1d05770373ab64b2",
+    "url": "/UolTest/static/media/logos.c4dc884d.png"
+  }
+]);
